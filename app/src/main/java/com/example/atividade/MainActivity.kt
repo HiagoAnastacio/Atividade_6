c62babe6af001c640ea52519918fbd2ca7c64ea8
Hiago A. dos Santos <159261628+HiagoAnastacio@users.noreply.github.com>
2025-03-10 16:34:00 -0300

    10/03 1.1

--- app/src/main/java/com/example/atividade/MainActivity.kt
+++ app/src/main/java/com/example/atividade/MainActivity.kt
@@ -42,104 +42,106 @@ fun CartaoDeVisita() {
     val telefoneUsuario = "(11) 98765-4321" // Telefone do usuário
     val redeSocialUsuario = "@joaoGTAbomgg" // Nome da rede social do usuário
     val emailUsuario = "joao.silva@exemplo.com" // Email do usuário
+    val backgroundcolor = Color(0XEE000033)
 
     // Cor do texto
     val textoCor = Color.Black // Cor que pode ser alterada conforme necessário
-    //val textDiff = Color.#3DDC84
+    val textDiff = Color(61, 180, 132)
 
     // Layout do cartão de visita
     Column( // Usamos um Column para empilhar os elementos verticalmente
         modifier = Modifier // Modificador para aplicar estilos e comportamentos
             .fillMaxSize() // Faz o Column ocupar todo o espaço disponível
-            .background(Color(144,238,144	)) // Define a cor de fundo como verde claro
-            .padding(15.dp), // Adiciona um preenchimento de 15 dp ao redor do Column
+            .background(Color(144, 238, 144)) // Define a cor de fundo como verde claro
+            .padding(15.dp, top = 200.dp, end = 15.dp), // Adiciona um preenchimento de 15 dp ao redor do Column
         horizontalAlignment = Alignment.CenterHorizontally // Alinha os elementos horizontalmente ao centro
     ) {
         // Logo
-        Image(
-            painter = painterResource(id = R.drawable.android_logo), // Carrega a imagem da logo a partir dos recursos
-            contentDescription = "Logo", // Descrição da imagem para acessibilidade
-            modifier = Modifier.size(100.dp), // Define o tamanho da imagem como 100 dp
-            contentScale = ContentScale.Crop // Escala a imagem para preencher o espaço disponível
-        )
+        Box (modifier = Modifier
+            .background(backgroundcolor)){
+            Image(
+                painter = painterResource(id = R.drawable.android_logo), // Carrega a imagem da logo a partir dos recursos
+                contentDescription = "Logo", // Descrição da imagem para acessibilidade
+                modifier = Modifier.size(100.dp), // Define o tamanho da imagem como 100 dp
+                contentScale = ContentScale.Crop // Escala a imagem para preencher o espaço disponível
+
+            )
+        }
 
         // Nome do Usuário
         Text(
             text = nomeUsuario,
-            style = MaterialTheme.typography.titleLarge.copy(color = textoCor, fontSize = 24.sp) // Exibe o nome do usuário com cor e tamanho personalizados
+            style = MaterialTheme.typography.titleLarge.copy(color = textoCor, fontSize = 35.sp) // Exibe o nome do usuário com cor e tamanho personalizados
         )
 
-        Spacer(modifier = Modifier.height(8.dp)) // Adiciona um espaço de 8 dp entre os elementos
+        Spacer(modifier = Modifier.height(4.dp)) // Adiciona um espaço de 8 dp entre os elementos
 
         // Cargo do Usuário
         Text(
             text = cargoUsuario,
-            style = MaterialTheme.typography.bodyMedium.copy(color = textoCor, fontSize = 15.sp) // Exibe o cargo do usuário com cor e tamanho personalizados
+            style = MaterialTheme.typography.bodyMedium.copy(color = textDiff, fontSize = 20.sp) // Exibe o cargo do usuário com cor e tamanho personalizados
         )
 
-        Spacer(modifier = Modifier.height(8.dp)) // Adiciona um espaço de 8 dp entre os elementos
+        Spacer(modifier = Modifier.height(4.dp)) // Adiciona um espaço de 8 dp entre os elementos
 
         // Telefone
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 2.dp) // Adiciona um preenchimento vertical de 4 dp
+                .padding(top = 220.dp, start = maxOf(100.dp)/*105.dp*/) // Adiciona um preenchimento vertical de 4 dp
         ) {
             Image(
                 painter = painterResource(id = R.drawable.phone_24dp_1f1f1f), // Carrega o ícone de telefone a partir dos recursos
                 contentDescription = "Telefone", // Descrição do ícone para acessibilidade
                 modifier = Modifier
                     .size(20.dp) // Define o tamanho do ícone como 24 dp
-                    .clickable { /* Ação ao clicar no ícone de telefone */ }
             )
             Text(
                 text = telefoneUsuario,
                 style = MaterialTheme.typography.bodyMedium.copy(color = textoCor, fontSize = 15.sp), // Exibe o telefone do usuário com cor e tamanho personalizados
-                modifier = Modifier.padding(15.dp) // Adiciona um preenchimento à esquerda do texto
+                modifier = Modifier.padding(start = 15.dp) // Adiciona um preenchimento à esquerda do texto
             )
         }
 
-        Spacer(modifier = Modifier.height(8.dp)) // Adiciona um espaço de 8 dp entre os elementos
+        Spacer(modifier = Modifier.height(4.dp)) // Adiciona um espaço de 8 dp entre os elementos
 
         // Rede Social
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 4.dp) // Adiciona um preenchimento vertical de 4 dp
+                .padding(top = 2.dp, start = maxOf(100.dp)) // Adiciona um preenchimento vertical de 4 dp
         ) {
             Image(
                 painter = painterResource(id = R.drawable.share_24dp_1f1f1f), // Carrega o ícone da rede social a partir dos recursos
                 contentDescription = "Rede Social", // Descrição do ícone para acessibilidade
                 modifier = Modifier
                     .size(20.dp) // Define o tamanho do ícone como 24 dp
-                    .clickable { /* Ação ao clicar no ícone da rede social */ }
             )
             Text(
                 text = redeSocialUsuario,
-                style = MaterialTheme.typography.bodyMedium.copy(color = textoCor, fontSize = 10.sp), // Exibe o nome da rede social do usuário com cor e tamanho personalizados
-                modifier = Modifier.padding(15.dp) // Adiciona um preenchimento à esquerda do texto
+                style = MaterialTheme.typography.bodyMedium.copy(color = textoCor, fontSize = 15.sp), // Exibe o nome da rede social do usuário com cor e tamanho personalizados
+                modifier = Modifier.padding(start = 15.dp) // Adiciona um preenchimento à esquerda do texto
             )
         }
 
-        Spacer(modifier = Modifier.height(8.dp)) // Adiciona um espaço de 8 dp entre os elementos
+        Spacer(modifier = Modifier.height(4.dp)) // Adiciona um espaço de 8 dp entre os elementos
 
         // Email
         Row(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 3.dp) // Adiciona um preenchimento vertical de 4 dp
+                .padding(top = 2.dp, start = maxOf(100.dp)) // Adiciona um preenchimento vertical de 4 dp
         ) {
             Image(
                 painter = painterResource(id = R.drawable.email_24dp_1f1f1f), // Carrega o ícone de email a partir dos recursos
                 contentDescription = "Email", // Descrição do ícone para acessibilidade
                 modifier = Modifier
                     .size(20.dp) // Define o tamanho do ícone como 24 dp
-                    .clickable { /* Ação ao clicar no ícone de email */ }
             )
             Text(
                 text = emailUsuario,
                 style = MaterialTheme.typography.bodyMedium.copy(color = textoCor, fontSize = 15.sp), // Exibe o email do usuário com cor e tamanho personalizados
-                modifier = Modifier.padding(15.dp) // Adiciona um preenchimento à esquerda do texto
+                modifier = Modifier.padding(start = 15.dp) // Adiciona um preenchimento à esquerda do texto
             )
         }
     }
